148481d9f7f588b71ffd4404a4df0f6bf6a88e2d
我想有个家 <niuniu5100200@163.com>
2020-04-10 15:48:56 +0800

    test

---
+++ src/Client.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 我想有个家
+ * Date: 2020/4/10
+ * Time: 15:39
+ */
+
+namespace mark0325\hjpush;
+
+
+class Client {
+    public function index(){
+        echo 123;
+    }
+}
