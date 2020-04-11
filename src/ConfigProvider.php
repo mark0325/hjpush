a292770cbdd63608abf89ee431f3df024b287199
我想有个家 <niuniu5100200@163.com>
2020-04-11 08:59:27 +0800

    test

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -25,13 +25,7 @@ class ConfigProvider
             // 合并到  config/autoload/dependencies.php 文件
             'dependencies' => [],
             // 合并到  config/autoload/annotations.php 文件
-            'annotations' => [
-                'scan' => [
-                    'paths' => [
-                        __DIR__,
-                    ],
-                ],
-            ],
+            'annotations' => [],
             // 默认 Command 的定义，合并到 Hyperf\Contract\ConfigInterface 内，换个方式理解也就是与 config/autoload/commands.php 对应
             'commands' => [],
             // 与 commands 类似
